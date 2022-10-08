9d810b9e41e8602590ca586208c7b6d5acd14f3b
Nik Medvedev <medvedevklaus@gmail.com>
2022-10-08 16:57:01 +0300

    feat: простой компонент приглашения пользователей

---
+++ src/App.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+function App() {
+    const [users, setUsers] = useState([]);
+    const [invites, setInvites] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+	const [success, setSuccess] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+
+    useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            .then(res => res.json())
+            .then(json => {
+                setUsers(json.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    const onChangeSearchValue = (event) => {
+        setSearchValue(event.target.value);
+    };
+
+    const onClickInvite = (id) => {
+        if (invites.includes(id)) {
+            setInvites(prev => prev.filter(_id => _id !== id));
+        } else {
+            setInvites(prev => [...prev, id]);
+        }
+    }
+
+	const onClickSendInvites = () => {
+		setSuccess(true);
+	}
+
+    return (
+        <div className="App">
+        {
+			success ? (
+				<Success count={invites.length}/>
+			) : (
+				<Users 
+					onChangeSearchValue={onChangeSearchValue}
+					onClickInvite={onClickInvite}
+					onClickSendInvites={onClickSendInvites}
+					invites={invites}
+					items={users} 
+					isLoading={isLoading}
+					searchValue={searchValue}  />
+			)
+		}
+        </div>
+    );
+}
+
+export default App;
